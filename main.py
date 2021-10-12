7183b23417e1ea07a98d23573bcffd77e7c9baad
chippoman <c.hippoman@gmail.com>
2021-10-12 17:18:41 -0400

    Search for last 'eth pool' string instead of just last message

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ api_hash = config['Telegram']['api_hash']
 client = TelegramClient(sessionStorage, api_id, api_hash)
 
 async def main():
-    async for message in client.iter_messages(-1001368873436, from_user='mining_pools_monitor_bot', limit=1, reverse=False):
+    async for message in client.iter_messages(-1001368873436, from_user='mining_pools_monitor_bot', search='eth pool', limit=1, reverse=False):
         last_id = config['Telegram']['last_id']
 
-        if (message.id > int(last_id) and "eth pool" in message.text):
+        if (message.id > int(last_id)):
             bot = discum.Client(token=config['Discord']['token'])
             config['Telegram']['last_id'] = str(message.id)
-            with open(configFile, 'w') as configOutput:
+            with open((configFile), 'w') as configOutput:
                 config.write(configOutput)
             bot.sendMessage(config['Discord']['channel_id'], message.text)
    
