a8cc694e5bddc0031df9a1f5ad36e5cd6f1e3f54
chippoman <c.hippoman@gmail.com>
2021-10-11 23:08:54 -0400

    Implemented absolute path for session storage file. Store last message id seen from bot

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from telethon import TelegramClient
 import discum
 import configparser
+import os
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+cwd = os.path.dirname(os.path.realpath('__file__'))
+sessionStorage = os.path.join(cwd, 'anon.session')
 
-# Remember to use your own values from my.telegram.org!
 api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
-client = TelegramClient('anon', api_id, api_hash)
-
-bot = discum.Client(token=config['Discord']['token'])
+client = TelegramClient(sessionStorage, api_id, api_hash)
 
 async def main():
     async for message in client.iter_messages(-1001368873436, from_user='mining_pools_monitor_bot', limit=1, reverse=False):
-        print(message.text)
-        print(message.id)
-        print(message.date)
+        last_id = config['Telegram']['last_id']
 
-        bot.sendMessage(config['Discord']['channel_id'], message.text)
+        if (message.id > int(last_id)):
+            bot = discum.Client(token=config['Discord']['token'])
+            config['Telegram']['last_id'] = str(message.id)
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            bot.sendMessage(config['Discord']['channel_id'], message.text)
    
 with client:
     client.loop.run_until_complete(main())
